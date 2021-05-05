b4cca9ff901a92fcd5b44c6a8927c9cb92006695
bacikgoz <berk.acikgoz@hepsiburada.com>
2021-05-05 23:47:35 +0300

    #65 - Finished pipes.

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  onNameChange(value: string): void {
+    this.name = value;
+  }
+
+  onDateChange(value: string): void {
+    this.date = value;
+  }
+
+  onAmountChange(value: string): void {
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(value: string): void {
+    this.height = parseFloat(value);
+  }
+
+  onMilesChange(value: string): void {
+    this.miles = parseFloat(value);
+  }
+}
