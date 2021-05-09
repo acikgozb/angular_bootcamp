9de8a9549ee8b06b2599b32cc2ea8c520d895f63
bacikgoz <berk.acikgoz@hepsiburada.com>
2021-05-09 23:12:10 +0300

    #8 - Routing and Navigation Between Pages

--- components/src/app/app-routing.module.ts
+++ components/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**', // wildcard, no matter which route is, render this component.
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
