280470c583aac34fcabe122d2f5b69f36787b032
bacikgoz <berk.acikgoz@hepsiburada.com>
2021-05-12 14:08:06 +0300

    #9 - Lazy Loading with Modules

--- components/src/app/collections/collections-home/collections-home.component.ts
+++ components/src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'Berk', age: 25, job: 'Developer', employed: true},
+    { name: 'Yaren', age: 23, job: 'Veterinarian', employed: true},
+    { name: 'Keeva', age: 6, job: 'Cat', employed: false}
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name'},
+    { key: 'age', label: 'Age'},
+    { key: 'job', label: 'Job'}
+  ];
 
   constructor() { }
 
